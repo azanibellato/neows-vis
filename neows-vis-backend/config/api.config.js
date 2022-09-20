8fc1eed072ae65e1f4a437a1b45f62cc3c860ca5
Andrea <andrea.zanibellato@gmail.com>
2022-09-21 00:14:38 +0200

    Added script for fetching data

---
+++ neows-vis-backend/config/api.config.js
@@ -0,0 +1,5 @@
+// Insert here your API key for NASA Api
+
+module.exports = {
+    API_KEY: "DEMO_KEY"
+};
--- neows-vis-backend/models/neows.model.js
+++ neows-vis-backend/models/neows.model.js
@@ -1,18 +1,51 @@
 const db = require("./db");
 
-function Neows(){
-    this.getWeek = function(callback){
-        db.query("SELECT * FROM `neows` WHERE `approach_date` between date_sub(now(),INTERVAL 1 WEEK) and now()", (err,res) => {
-        
+function Neows(nasa_id, neos_reference_id, name, approach_date, diameter, magnitude, distance, velocity){
+    this.nasa_id = nasa_id;
+    this.neos_reference_id = neos_reference_id;
+    this.name = name;
+    this.approach_date = approach_date;
+    this.diameter = diameter;
+    this.magnitude = magnitude;
+    this.distance = distance;
+    this.velocity = velocity;
+}
+
+Neows.getWeek = function(callback){
+    db.query("SELECT * FROM `neows` WHERE `approach_date` between date_sub(now(),INTERVAL 1 WEEK) and now()", (err,res) => {
+    
+    if (err) {
+        console.log("error:", err);
+        callback(err, null);
+        return;
+    }
+    console.log(res);
+    callback(null, res);
+    });
+};
+
+Neows.insertData = function(nasa_data, callback){
+    console.log(nasa_data);
+    const table_data = nasa_data.map(el => 
+        [   el.id, 
+            el.neos_reference_id, 
+            el.name, 
+            el.close_approach_data[0].close_approach_date,
+            (el.estimated_diameter.kilometers.estimated_diameter_min+el.estimated_diameter.kilometers.estimated_diameter_max)/2,
+             el.absolute_magnitude_h, 
+             el.close_approach_data[0].miss_distance.astronomical, 
+             el.close_approach_data[0].relative_velocity.kilometers_per_second
+        ]
+    );
+    db.query("INSERT IGNORE INTO `neows` (nasa_id, neos_reference_id, name, approach_date, diameter, magnitude, distance, velocity) VALUES ?", [table_data], (err,res)=>{
         if (err) {
-          console.log("error:", err);
-          callback(err, null);
-          return;
+            console.log("error:", err);
+            callback(err, null);
+            return;
         }
         console.log(res);
         callback(null, res);
-      });
-    }
+    })
 
 }
 
