43bf0e641c33b66e9c605115fea7dbb89f99a188
Andrea <andrea.zanibellato@gmail.com>
2022-09-21 22:13:43 +0200

    Completed side graph

--- neows-vis-frontend/src/graphs/side-graph.js
+++ neows-vis-frontend/src/graphs/side-graph.js
@@ -6,7 +6,7 @@ export function buildSideGraph(dataArray){
 
     d3.selectAll("#side-graph > *").remove();
 
-    const margin = {top: 20, right: 20, bottom: 30, left: 50},
+    const margin = {top: 20, right: 20, bottom: 30, left: 30},
         width = 300 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
@@ -21,6 +21,8 @@ export function buildSideGraph(dataArray){
     var size = d3.scaleLinear()
         .domain(d3.extent(dataArray, d=>d.magnitude))
         .range([ 1,  MAX_SIZE]);
+
+   
 
     
     svg.append('g')
@@ -42,5 +44,32 @@ export function buildSideGraph(dataArray){
         .attr("y", (d, i)=> i*80+MAX_SIZE/2-size(d.magnitude)/2)
         .attr("transform", (d, i)=>"rotate(45 0 "+(i*80)+")")
     
+    svg.append('g')
+        .selectAll("dot").data(dataArray).enter().append("circle")
+        .attr("cx", 0 )
+        .attr("cy", (d, i)=> i*80+(MAX_SIZE/2)+7 )
+        .attr("r", 1 )
+        .attr("fill", "black")
+
+    svg.selectAll("text")
+        .data(dataArray).enter()
+        .append("text")
+        .attr("x", 0 )
+        .attr("y", (d, i)=> i*80+5)
+        .attr("fill", "white")
+        .attr("class","side-label")
+        .append('svg:tspan')
+        .attr('x', 50)
+        .attr('dy', 5)
+        .text(d => `Name: ${d.name}`)
+        .append('svg:tspan')
+        .attr('x', 50)
+        .attr('dy', 20)
+        .text(d => `Diameter: ${d.diameter} km`)
+        .append('svg:tspan')
+        .attr('x', 50)
+        .attr('dy', 20)
+        .text(d => `Magnitude: ${d.magnitude} h`)
+        
 
 }
